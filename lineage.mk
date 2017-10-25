fea0dde41f2673fe8ce2d5b50b6850487888ed07
podkopaew <podkopaew@list.ru>
2017-10-25 22:44:28 +0300

    Update and rename cm.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -2,14 +2,14 @@
 PRODUCT_RELEASE_NAME := X5
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_cm_phone.mk)
+$(call inherit-product, vendor/cm/config/common_lineage_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Doogee/X5/cm_X5.mk)
+$(call inherit-product, device/Doogee/X5/lineage_X5.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X5
-PRODUCT_NAME := cm_X5
+PRODUCT_NAME := lineage_X5
 PRODUCT_BRAND := Doogee
 PRODUCT_MODEL := Doogee X5
 PRODUCT_MANUFACTURER := Doogee
